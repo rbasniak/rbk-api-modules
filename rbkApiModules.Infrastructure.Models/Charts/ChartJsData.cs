e346cba9d56ca120f7d8901ce217519f39a1cb5c
Rodrigo Basniak <rbasniak@gmail.com>
2020-11-30 16:44:02 -0300

    chore: updated ChartJs models

--- rbkApiModules.Infrastructure.Models/Charts/ChartJsData.cs
+++ rbkApiModules.Infrastructure.Models/Charts/ChartJsData.cs
@@ -17,7 +17,7 @@ namespace rbkApiModules.Infrastructure.Models.Charts.ChartJs
         public ChartJsData(string title) : this()
         {
             Options.Title.Text = title;
-        }
+        } 
 
         public ChartJsData(string id, string title): this(title)
         {
@@ -27,5 +27,43 @@ namespace rbkApiModules.Infrastructure.Models.Charts.ChartJs
         public string Id { get; set; }
         public ChartJsChartData<T> Data { get; set; }
         public ChartJsChartOptions Options { get; set; }
+
+        public void AddSeries(string name, DateTime startDate, DateTime endDate, string borderColor, string backgroundColor, double lineTension = 0)
+        {
+            var dataset = new ChartJsDataset<T>();
+            var axisData = BuildLineChartAxis(startDate, endDate);
+
+            dataset.Label = name;
+            dataset.LineTension = lineTension;
+            dataset.BackgroundColor = backgroundColor;
+            dataset.BorderColor = borderColor;
+
+            foreach (var item in axisData)
+            {
+                dataset.Data.Add(new ChartPoint<T>((T)((object)item), 0, null));
+            }
+
+            Data.Datasets.Add(dataset);
+        }
+
+        private List<ChartPoint<DateTime>> BuildLineChartAxis(DateTime startDate, DateTime endDate)
+        {
+            var date = startDate;
+
+            var axis = new List<ChartPoint<DateTime>>();
+
+            while (date <= endDate.Date)
+            {
+                axis.Add(new ChartPoint<DateTime>(date.Date, 0));
+                date = date.AddDays(1);
+            }
+
+            if (axis.Last().X.Date != endDate.Date)
+            {
+                axis.Add(new ChartPoint<DateTime>(endDate.Date, 0));
+            }
+
+            return axis;
+        }
     }
 }
