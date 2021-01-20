5ba3777142a92425dd3ecd83352af033304a9643
Rodrigo Basniak <rbasniak@gmail.com>
2021-01-19 21:29:47 -0300

    chore: fixed demo project

--- rbkApiModules.Demo/Models/States/Document.cs
+++ rbkApiModules.Demo/Models/States/Document.cs
@@ -8,5 +8,9 @@ namespace rbkApiModules.Demo.Models.StateMachine
 {
     public class Document : BaseStateEntity<State, Event, Transition, Document, StateChangeEvent, StateGroup, QueryDefinitionGroup, QueryDefinition, QueryDefinitionToState, QueryDefinitionToGroup>
     {
+        protected override StateChangeEvent CreateStateChangeEvent(string user, Transition transition)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
