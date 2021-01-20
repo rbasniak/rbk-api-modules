dbdc22ec3da6ac73f1e5ca6844e7f2d578e71ee2
Rodrigo Basniak <rbasniak@gmail.com>
2021-01-19 21:22:09 -0300

    chore: added status change method

--- rbkApiModules.Workflow/Models/BaseStateEntity.cs
+++ rbkApiModules.Workflow/Models/BaseStateEntity.cs
@@ -1,4 +1,5 @@
-﻿using rbkApiModules.Infrastructure.Models;
+﻿using Microsoft.EntityFrameworkCore;
+using rbkApiModules.Infrastructure.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -35,5 +36,53 @@ namespace rbkApiModules.Workflow
         public virtual TState State { get; protected set; }
 
         public virtual IEnumerable<TStateChangeEvent> Events => _events?.ToList();
+
+        public virtual void NextStatus(TEvent trigger, string user, DbContext context)
+        {
+            if (State == null)
+            {
+                context.Entry(this).Reference(x => x.State).Load();
+            }
+
+            if (State.Transitions == null)
+            {
+                context.Entry(State).Collection(x => x.Transitions).Load();
+            }
+
+            foreach (var child in State.Transitions)
+            {
+                if (child.FromState == null) context.Entry(child).Reference(x => x.FromState).Load();
+                if (child.ToState == null) context.Entry(child).Reference(x => x.ToState).Load();
+                if (child.Event == null) context.Entry(child).Reference(x => x.Event).Load();
+            }
+
+            TTransition transition = null;
+
+            var transitionHistory = String.Empty;
+
+            transition = State.Transitions.FirstOrDefault(x => x.Event.Id == trigger.Id);
+
+            if (transition != null)
+            {
+                State = transition.ToState;
+                transitionHistory = transition.History;
+
+                ExecuteDomainSpecificActions(user, transition);
+            }
+            else
+            {
+                throw new SafeException("Mudança de status inválida para a entidade selecionada");
+            }
+
+            var @event = CreateStateChangeEvent(user, transition);
+            _events.Add(@event);
+        }
+
+        protected virtual void ExecuteDomainSpecificActions(string user, TTransition transition)
+        {
+
+        }
+
+        protected abstract TStateChangeEvent CreateStateChangeEvent(string user, TTransition transition); 
     }
 }
