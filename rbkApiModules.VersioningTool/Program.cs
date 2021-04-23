47244f9ae5eec860036af11156daf5c4a2332b68
Rodrigo Basniak <rbasniak@gmail.com>
2021-04-22 22:04:27 -0300

    deactivating ci

---
+++ rbkApiModules.VersioningTool/Program.cs
@@ -0,0 +1,32 @@
+﻿using LibGit2Sharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace rbkApiModules.VersioningTool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var repo = new Repository(@"D:\Repositories\pessoal\libraries\rbk-api-modules-next"))
+            {
+                var lastTag = repo.Tags.OrderByDescending(x => ((Commit)x.Target).Committer.When).First();
+                var allCommits = repo.Commits.OrderByDescending(x => x.Committer.When).ToList();
+
+                var messages = new List<string>();
+                
+                foreach (var commit in allCommits)
+                {
+                    if (commit.Id == lastTag.Target.Id) break;
+
+                    messages.Add(commit.Message);
+                    Console.WriteLine(commit.Message);
+                }
+
+                Console.WriteLine("Done.");
+                Console.ReadKey();
+            }
+        }
+    }
+}
