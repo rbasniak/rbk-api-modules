fd275acd53b824d18372cfefa640c8da30e2ca54
Rodrigo Basniak <rbasniak@gmail.com>
2021-05-19 21:54:51 -0300

    chore: added new generic TreeNode

--- rbkApiModules.Infrastructure.Models/DataTransfer/TreeNode.cs
+++ rbkApiModules.Infrastructure.Models/DataTransfer/TreeNode.cs
@@ -20,4 +20,23 @@ namespace rbkApiModules.Infrastructure.Models
         public bool Expanded { get; set; }
         public List<TreeNode> Children { get; set; }
     }
+
+    public class TreeNode<T>
+    {
+        public string Label { get; set; }
+        public object Data { get; set; }
+        public string Icon { get; set; }
+        public string ExpandedIcon { get; set; }
+        public string CollapsedIcon { get; set; }
+        public bool Leaf { get; set; }
+        public string Type { get; set; }
+        public string Style { get; set; }
+        public string StyleClass { get; set; }
+        public bool Draggable { get; set; }
+        public bool Droppable { get; set; }
+        public bool Selectable { get; set; }
+        public string Key { get; set; }
+        public bool Expanded { get; set; }
+        public List<T> Children { get; set; }
+    } 
 }
