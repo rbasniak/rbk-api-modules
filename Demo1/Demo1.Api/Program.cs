54d78847b38c545d1d1939690370d419ef1ce9c7
Rodrigo Basniak <rodrigo.basniak@aveva.com>
2023-01-23 15:20:34 +0100

    Testing log configuration

--- Demo1/Demo1.Api/Program.cs
+++ Demo1/Demo1.Api/Program.cs
@@ -10,14 +10,21 @@ public class Program
     public static int Main(string[] args)
     {
         Log.Logger = new LoggerConfiguration()
-            .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+            .MinimumLevel.Verbose()
             .Enrich.FromLogContext()
             .Enrich.WithExceptionDetails()
-            .WriteTo.Console()
-            .WriteTo.Seq("http://localhost:5341", LogEventLevel.Information)
+            .WriteTo.Conditional(@event => @event.Level >= LogEventLevel.Error, w => w.Console())
+            // .WriteTo.Seq("http://localhost:5341", LogEventLevel.Information)
             .WriteTo.File(Path.Combine(Environment.CurrentDirectory, "Logs", "log.txt"), LogEventLevel.Debug, fileSizeLimitBytes: 1024 * 1024, shared: true, rollOnFileSizeLimit: true, rollingInterval: RollingInterval.Day)
             .WriteTo.File(new JsonFormatter(renderMessage: true), Path.Combine(Environment.CurrentDirectory, "Logs", "log.json"), LogEventLevel.Debug, fileSizeLimitBytes: 1024 * 1024, shared: true, rollOnFileSizeLimit: true, rollingInterval: RollingInterval.Day)
             .CreateLogger();
+
+        Log.Logger.Verbose("VERBOSE DATA");
+        Log.Logger.Debug("DEBUG DATA");
+        Log.Logger.Information("INFORMATION DATA");
+        Log.Logger.Warning("WARNING DATA");
+        Log.Logger.Error("ERROR DATA");
+        Log.Logger.Fatal("FATAL DATA");
 
         try
         {
