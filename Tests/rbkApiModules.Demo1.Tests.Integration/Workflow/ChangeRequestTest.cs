d2946b5882dc5a6f0e786a55a3940c2fb1cacf01
Rodrigo Basniak <rbasniak@gmail.com>
2023-04-21 14:42:05 +0200

    WIP on workflow library

--- Tests/rbkApiModules.Demo1.Tests.Integration/Workflow/ChangeRequestTest.cs
+++ Tests/rbkApiModules.Demo1.Tests.Integration/Workflow/ChangeRequestTest.cs
@@ -23,52 +23,56 @@ public class ChangeRequestWorkflow
 
     public enum Trigger
     {
-        CRIAR_ATRAVES_DO_REQUISITANTE,
-        CRIAR_ATRAVES_DO_ADMINISTRADOR,
-        ENVIAR_PARA_AVALIACAO,
-        CANCELAR,
-        AVALIAR_SOLICITACAO_PELO_ADMINISTRADOR,
-        SOLICITAR_INFORMACOES_ADICIONAIS,
-        APROVAR_AVALIACAO_FINAL,
-        AVALIAR_PELO_TECNICO,
-        APROVAR_PARA_EXECUCAO,
-        INICIAR_EXECUCAO,
-        CONCLUIR_EXECUCAO,
-        INICIAR_ANALISE_DA_EXECUCAO_PELO_SOLICITANTE,
-        REPROVAR_PELO_SOLICITANTE,
-        APROVAR_PELO_SOLICITANTE,
-        INICIAR_ANALISE_DA_EXECUCAO_PELO_ADMINISTRADOR,
-        REPROVAR_PELO_ADMINISTRADOR,
-        APROVAR_PELO_ADMINISTRADOR,
-        DESIGNAR_TECNICO_DE_DOCUMENTACAO,
-        INICIAR_REVISAO,
-        CONCLUIR_CADASTRO_SINDOTEC,
-        CONCLUIR_REVISAO,
-        INICIAR_ANALISE_DA_EXECUCAO_PELO_TECNICO
+        APROVAR_AVALIACAO_FINAL = 1,
+        APROVAR_PARA_EXECUCAO = 2,
+        APROVAR_PELO_ADMINISTRADOR = 3,
+        APROVAR_PELO_SOLICITANTE = 4,
+        CANCELAR = 5,
+        CONCLUIR_CADASTRO_SINDOTEC = 6,
+        CONCLUIR_EXECUCAO = 7,
+        CONCLUIR_REVISAO = 8,
+        CRIAR_ATRAVES_DO_ADMINISTRADOR = 9,
+        CRIAR_ATRAVES_DO_REQUISITANTE = 10,
+        DESIGNAR_TECNICO_DE_DOCUMENTACAO = 11,
+        ENVIAR_PARA_AVALIACAO_DA_EXECUCAO = 12,
+        ENVIAR_PARA_AVALIACAO_INICIAL = 13,
+        INICIAR_ANALISE_DA_EXECUCAO_PELO_ADMINISTRADOR = 14,
+        INICIAR_ANALISE_DA_EXECUCAO_PELO_SOLICITANTE = 15,
+        INICIAR_ANALISE_PARA_EXECUCAO_PELO_TECNICO = 16,
+        INICIAR_AVALIACAO_DA_EXECUCAO_PELO_ADMINISTRADOR = 17,
+        INICIAR_AVALIACAO_DA_EXECUCAO_PELO_TECNICO = 18,
+        INICIAR_AVALIACAO_INICIAL = 19,
+        INICIAR_EXECUCAO = 20,
+        INICIAR_REVISAO = 21,
+        REPROVAR_EXECUCAO_PELO_ADMINISTRADOR = 22,
+        REPROVAR_EXECUCAO_PELO_SOLICITANTE = 23,
+        SOLICITAR_INFORMACOES_ADICIONAIS_PARA_ANALISE_INICIAL = 24,
+        SOLICITAR_INFORMACOES_ADICIONAIS_PARA_EXECUCAO = 25,
+        SOLICITAR_INFORMACOES_ADICIONAIS_PARA_REVISAO = 26,
     }
 
     public enum State
     {
-        CRIADA_NO_SISTEMA_PELO_REQUISITANTE,
-        CRIADA_NO_SISTEMA_PELO_ADMINISTRADOR,
-        RASCUNHO,
-        AGUARDANDO_AVALIACAO_DO_ADMINISTRADOR,
-        EM_AVALIACAO_PELO_ADMINISTRADOR,
-        AGUARDANDO_INFORMACOES_EXTRAS_DA_AVALIACAO_INICIAL_SOLICITADAS_PELO_ADMINISTRADOR,
-        AGUARDANDO_AVALIACAO_DO_TECNICO,
-        EM_AVALIACAO_PELO_TECNICO,
-        AGUARDANDO_EXECUCAO,
-        AguardandoInformacoesExtrasDaAvaliacaoInicialSolicitadasPeloTecnico,
-        EM_EXECUCAO,
-        AGUARDANDO_ANALISE_DA_EXECUCAO_PELO_SOLICITANTE,
-        EmAnaliseDaExecucaoPeloSolicitante,
-        EmAnaliseDaExecucaoPeloAdministrador,
-        AguardandoCadastroNoSindotec,
-        AguardandoRevisao,
-        EmRevisao,
-        CANCELADA,
-        AguardandoAvaliacaoDaExecucaoPeloAdministrador,
-        Concluded,
+        AGUARDANDO_AVALIACAO_DA_EXECUCAO_PELO_SOLICITANTE = 1,
+        AGUARDANDO_AVALIACAO_DA_EXECUCAO_PELO_ADMINISTRADOR = 2,
+        AGUARDANDO_AVALIACAO_DO_PEDIDO_PELO_ADMINISTRADOR = 3,
+        AGUARDANDO_AVALIACAO_DO_PEDIDO_PELO_TECNICO_EXECUTANTE = 4,
+        AGUARDANDO_CADASTRO_NO_SINDOTEC = 5,
+        AGUARDANDO_EXECUCAO = 6,
+        AGUARDANDO_INFORMACOES_EXTRAS_PARA_AVALIACAO_INICIAL = 7,
+        AGUARDANDO_INFORMACOES_EXTRAS_PARA_AVALIACAO_DO_TECNICO = 8,
+        AGUARDANDO_REVISAO = 9,
+        CANCELADA = 10,
+        CONCLUIDA = 11,
+        CRIADA_NO_SISTEMA_PELO_ADMINISTRADOR = 12,
+        CRIADA_NO_SISTEMA_PELO_REQUISITANTE = 13,
+        EXECUCAO_EM_ANALISE_PELO_SOLICITANTE = 14,
+        EXECUCAO_EM_ANALISE_PELO_ADMINISTRADOR = 15,
+        EM_AVALIACAO_PELO_ADMINISTRADOR = 16,
+        EM_AVALIACAO_PELO_TECNICO_EXECUTANTE = 17,
+        EM_EXECUCAO = 18,
+        EM_REVISAO = 19,
+        RASCUNHO = 20,
     }
 
     public ChangeRequestWorkflow(FakeNotificationService notificationService)
@@ -83,7 +87,7 @@ public class ChangeRequestWorkflow
 
             //    return changeRequest.Id == "019";
             //})
-            .PermitIf(Trigger.APROVAR_PARA_EXECUCAO, State.AGUARDANDO_AVALIACAO_DO_ADMINISTRADOR, new GuardDefinition(String.Empty, (args) =>
+            .PermitIf(Trigger.APROVAR_PARA_EXECUCAO, State.AGUARDANDO_AVALIACAO_DO_PEDIDO_PELO_ADMINISTRADOR, new GuardDefinition(String.Empty, (args) =>
             {
                 var changeRequest = (ChangeRequest)args[0];
                 var date = (DateTime)args[1];
@@ -95,31 +99,31 @@ public class ChangeRequestWorkflow
             // .OnDeactivate(() => Debug.WriteLine($"State {State.RASCUNHO} deactivated"))
             .OnExit(transition => Debug.WriteLine($"Exited from {transition.Source} and entered {transition.Destination} by firing {transition.Trigger}"));
 
-        _machine.Configure(State.AGUARDANDO_AVALIACAO_DO_ADMINISTRADOR)
+        _machine.Configure(State.AGUARDANDO_AVALIACAO_DO_PEDIDO_PELO_ADMINISTRADOR)
             .Permit(Trigger.CANCELAR, State.CANCELADA)
             .OnEntry(transition => Debug.WriteLine($"Entered {transition.Destination} from {transition.Source} by firing {transition.Trigger}"))
             // .OnEntryFrom(Trigger.ENVIAR_PARA_AVALIACAO, (transition) => Debug.WriteLine($"Entered {transition.Destination} from {transition.Source} by firing ONLY {transition.Trigger}"))
             // .OnEntryFrom(_machine.SetTriggerParameters<ChangeRequest>(Trigger.ENVIAR_PARA_AVALIACAO), OnEnteredThisState)
 
-            .Permit(Trigger.SOLICITAR_INFORMACOES_ADICIONAIS, State.AGUARDANDO_INFORMACOES_EXTRAS_DA_AVALIACAO_INICIAL_SOLICITADAS_PELO_ADMINISTRADOR)
-            .Permit(Trigger.APROVAR_PELO_ADMINISTRADOR, State.AGUARDANDO_AVALIACAO_DO_TECNICO);
+            .Permit(Trigger.SOLICITAR_INFORMACOES_ADICIONAIS_PARA_ANALISE_INICIAL, State.AGUARDANDO_INFORMACOES_EXTRAS_PARA_AVALIACAO_INICIAL)
+            .Permit(Trigger.APROVAR_PELO_ADMINISTRADOR, State.AGUARDANDO_AVALIACAO_DO_PEDIDO_PELO_TECNICO_EXECUTANTE);
 
-        _machine.Configure(State.AGUARDANDO_INFORMACOES_EXTRAS_DA_AVALIACAO_INICIAL_SOLICITADAS_PELO_ADMINISTRADOR)
+        _machine.Configure(State.AGUARDANDO_INFORMACOES_EXTRAS_PARA_AVALIACAO_INICIAL)
             .Permit(Trigger.CANCELAR, State.CANCELADA)
-            .Permit(Trigger.ENVIAR_PARA_AVALIACAO, State.EM_AVALIACAO_PELO_ADMINISTRADOR);
+            .Permit(Trigger.ENVIAR_PARA_AVALIACAO_INICIAL, State.EM_AVALIACAO_PELO_ADMINISTRADOR);
 
-        _machine.Configure(State.AGUARDANDO_AVALIACAO_DO_TECNICO)
+        _machine.Configure(State.AGUARDANDO_AVALIACAO_DO_PEDIDO_PELO_TECNICO_EXECUTANTE)
             .Permit(Trigger.CANCELAR, State.CANCELADA)
-            .Permit(Trigger.INICIAR_ANALISE_DA_EXECUCAO_PELO_TECNICO, State.EM_AVALIACAO_PELO_TECNICO);
+            .Permit(Trigger.INICIAR_ANALISE_PARA_EXECUCAO_PELO_TECNICO, State.EM_AVALIACAO_PELO_TECNICO_EXECUTANTE);
 
-        _machine.Configure(State.EM_AVALIACAO_PELO_TECNICO)
+        _machine.Configure(State.EM_AVALIACAO_PELO_TECNICO_EXECUTANTE)
             .Permit(Trigger.CANCELAR, State.CANCELADA)
-            .Permit(Trigger.SOLICITAR_INFORMACOES_ADICIONAIS, State.AguardandoInformacoesExtrasDaAvaliacaoInicialSolicitadasPeloTecnico)
+            .Permit(Trigger.SOLICITAR_INFORMACOES_ADICIONAIS_PARA_EXECUCAO, State.AGUARDANDO_INFORMACOES_EXTRAS_PARA_AVALIACAO_DO_TECNICO)
             .Permit(Trigger.APROVAR_PARA_EXECUCAO, State.AGUARDANDO_EXECUCAO);
 
-        _machine.Configure(State.AguardandoInformacoesExtrasDaAvaliacaoInicialSolicitadasPeloTecnico)
+        _machine.Configure(State.AGUARDANDO_INFORMACOES_EXTRAS_PARA_AVALIACAO_DO_TECNICO)
             .Permit(Trigger.CANCELAR, State.CANCELADA)
-            .Permit(Trigger.ENVIAR_PARA_AVALIACAO, State.EM_AVALIACAO_PELO_TECNICO);
+            .Permit(Trigger.ENVIAR_PARA_AVALIACAO_DA_EXECUCAO, State.EM_AVALIACAO_PELO_TECNICO_EXECUTANTE);
 
         _machine.Configure(State.AGUARDANDO_EXECUCAO)
             .Permit(Trigger.CANCELAR, State.CANCELADA)
@@ -127,36 +131,36 @@ public class ChangeRequestWorkflow
 
         _machine.Configure(State.EM_EXECUCAO)
             .Permit(Trigger.CANCELAR, State.CANCELADA)
-            .Permit(Trigger.CONCLUIR_EXECUCAO, State.AguardandoAvaliacaoDaExecucaoPeloAdministrador)
+            .Permit(Trigger.CONCLUIR_EXECUCAO, State.AGUARDANDO_AVALIACAO_DA_EXECUCAO_PELO_ADMINISTRADOR)
             .OnEntryFrom(Trigger.INICIAR_EXECUCAO, (t) => Console.WriteLine(t.Parameters.Length))
             .OnEntry(transition => Console.WriteLine());
 
-        _machine.Configure(State.AguardandoAvaliacaoDaExecucaoPeloAdministrador)
+        _machine.Configure(State.AGUARDANDO_AVALIACAO_DA_EXECUCAO_PELO_ADMINISTRADOR)
             .Permit(Trigger.CANCELAR, State.CANCELADA)
-            .Permit(Trigger.INICIAR_ANALISE_DA_EXECUCAO_PELO_SOLICITANTE, State.EmAnaliseDaExecucaoPeloSolicitante);
+            .Permit(Trigger.INICIAR_ANALISE_DA_EXECUCAO_PELO_SOLICITANTE, State.EXECUCAO_EM_ANALISE_PELO_SOLICITANTE);
 
-        _machine.Configure(State.EmAnaliseDaExecucaoPeloSolicitante)
+        _machine.Configure(State.EXECUCAO_EM_ANALISE_PELO_SOLICITANTE)
             .Permit(Trigger.CANCELAR, State.CANCELADA)
-            .Permit(Trigger.APROVAR_PELO_SOLICITANTE, State.AguardandoAvaliacaoDaExecucaoPeloAdministrador)
-            .Permit(Trigger.REPROVAR_PELO_SOLICITANTE, State.AguardandoRevisao);
+            .Permit(Trigger.APROVAR_PELO_SOLICITANTE, State.AGUARDANDO_AVALIACAO_DA_EXECUCAO_PELO_ADMINISTRADOR)
+            .Permit(Trigger.REPROVAR_EXECUCAO_PELO_SOLICITANTE, State.AGUARDANDO_REVISAO);
 
-        _machine.Configure(State.AguardandoAvaliacaoDaExecucaoPeloAdministrador)
+        _machine.Configure(State.AGUARDANDO_AVALIACAO_DA_EXECUCAO_PELO_ADMINISTRADOR)
             .Permit(Trigger.CANCELAR, State.CANCELADA)
-            .Permit(Trigger.APROVAR_PELO_ADMINISTRADOR, State.Concluded)
-            .Permit(Trigger.REPROVAR_PELO_ADMINISTRADOR, State.AguardandoRevisao)
-            .Permit(Trigger.DESIGNAR_TECNICO_DE_DOCUMENTACAO, State.AguardandoCadastroNoSindotec);
+            .Permit(Trigger.APROVAR_PELO_ADMINISTRADOR, State.CONCLUIDA)
+            .Permit(Trigger.REPROVAR_EXECUCAO_PELO_ADMINISTRADOR, State.AGUARDANDO_REVISAO)
+            .Permit(Trigger.DESIGNAR_TECNICO_DE_DOCUMENTACAO, State.AGUARDANDO_CADASTRO_NO_SINDOTEC);
 
-        _machine.Configure(State.AguardandoCadastroNoSindotec)
+        _machine.Configure(State.AGUARDANDO_CADASTRO_NO_SINDOTEC)
             .Permit(Trigger.CANCELAR, State.CANCELADA)
-            .Permit(Trigger.CONCLUIR_CADASTRO_SINDOTEC, State.AguardandoAvaliacaoDaExecucaoPeloAdministrador);
+            .Permit(Trigger.CONCLUIR_CADASTRO_SINDOTEC, State.AGUARDANDO_AVALIACAO_DA_EXECUCAO_PELO_ADMINISTRADOR);
 
-        _machine.Configure(State.AguardandoRevisao)
+        _machine.Configure(State.AGUARDANDO_REVISAO)
             .Permit(Trigger.CANCELAR, State.CANCELADA)
-            .Permit(Trigger.INICIAR_REVISAO, State.EmRevisao);
+            .Permit(Trigger.INICIAR_REVISAO, State.EM_REVISAO);
 
-        _machine.Configure(State.EmRevisao)
+        _machine.Configure(State.EM_REVISAO)
             .Permit(Trigger.CANCELAR, State.CANCELADA)
-            .Permit(Trigger.CONCLUIR_REVISAO, State.AguardandoAvaliacaoDaExecucaoPeloAdministrador);
+            .Permit(Trigger.CONCLUIR_REVISAO, State.AGUARDANDO_AVALIACAO_DA_EXECUCAO_PELO_ADMINISTRADOR);
 
     }
 
