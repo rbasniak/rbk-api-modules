ba744ccae143cae065227ce254f4baeb6a518a98
Rodrigo Basniak <rbasniak@gmail.com>
2023-05-12 14:33:28 +0200

    WIP

---
+++ Libraries/rbkApiModules.CodeAnalysers/Analysers/OldNamespaceAnalyzer.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+using System.Collections.Immutable;
+
+namespace rbkApiModules.Commons.CodeAnalysers
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class OldNamespaceAnalyzer : DiagnosticAnalyzer
+    {
+        private const string Category = "Naming";
+
+        private static readonly DiagnosticDescriptor Rule = new DiagnosticDescriptor(
+            "OldNamespaceUsage",
+            "Using old style namespace (using brackets) is discouraged",
+            "Using old style namespace (using brackets) is discouraged. Please use using statement instead.",
+            Category,
+            DiagnosticSeverity.Error,
+            isEnabledByDefault: true);
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(Rule);
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+            context.EnableConcurrentExecution();
+
+            context.RegisterSyntaxNodeAction(AnalyzeNode, SyntaxKind.UsingDirective);
+        }
+
+        private static void AnalyzeNode(SyntaxNodeAnalysisContext context)
+        {
+            var diagnostic = Diagnostic.Create(Rule, Location.None);
+            context.ReportDiagnostic(diagnostic);
+            //var usingDirective = (UsingDirectiveSyntax)context.Node;
+
+            //if (usingDirective.StaticKeyword.IsKind(SyntaxKind.None) && usingDirective.Name.ToString().Contains("["))
+            //{
+            //    var diagnostic = Diagnostic.Create(Rule, usingDirective.GetLocation());
+            //    context.ReportDiagnostic(diagnostic);
+            //}
+        }
+    }
+}
--- Libraries/rbkApiModules.Diagnostics.Core/Diagnostics/DiagnosticsController.cs
+++ Libraries/rbkApiModules.Diagnostics.Core/Diagnostics/DiagnosticsController.cs
@@ -1,41 +1,41 @@
-﻿using MediatR;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Mvc;
-using rbkApiModules.Commons.Core.CodeGeneration;
-using rbkApiModules.Commons.Core;
+﻿//using MediatR;
+//using Microsoft.AspNetCore.Authorization;
+//using Microsoft.AspNetCore.Mvc;
+//using rbkApiModules.Commons.Core.CodeGeneration;
+//using rbkApiModules.Commons.Core;
 
-namespace rbkApiModules.Diagnostics.Core;
+//namespace rbkApiModules.Diagnostics.Core;
 
-[Authorize]
-[ApiController]
-[IgnoreOnCodeGeneration]
-[Route("api/[controller]")]
-public class DiagnosticsController : BaseController
-{
-    [HttpGet]
-    [Route("filter-options")]
-    public async Task<ActionResult<FilterDiagnosticsEntries>> GetFilterData()
-    {
-        var result = await Mediator.Send(new GetFilteringLists.Command());
+//[Authorize]
+//[ApiController]
+//[IgnoreOnCodeGeneration]
+//[Route("api/[controller]")]
+//public class DiagnosticsController : BaseController
+//{
+//    [HttpGet]
+//    [Route("filter-options")]
+//    public async Task<ActionResult<FilterDiagnosticsEntries>> GetFilterData()
+//    {
+//        var result = await Mediator.Send(new GetFilteringLists.Command());
 
-        return HttpResponse(result);
-    }
+//        return HttpResponse(result);
+//    }
 
-    [HttpPost]
-    [Route("search")]
-    public async Task<ActionResult<FilterDiagnosticsEntries.Results>> Search([FromBody] FilterDiagnosticsEntries.Command data)
-    {
-        var result = await Mediator.Send(data);
+//    [HttpPost]
+//    [Route("search")]
+//    public async Task<ActionResult<FilterDiagnosticsEntries.Results>> Search([FromBody] FilterDiagnosticsEntries.Command data)
+//    {
+//        var result = await Mediator.Send(data);
 
-        return HttpResponse(result);
-    }
+//        return HttpResponse(result);
+//    }
 
-    [HttpPost]
-    [Route("dashboard")]
-    public async Task<ActionResult<DiagnosticsDashboard>> GetDashboardData([FromBody] GetDashboardData.Command data)
-    {
-        var result = await Mediator.Send(data);
+//    [HttpPost]
+//    [Route("dashboard")]
+//    public async Task<ActionResult<DiagnosticsDashboard>> GetDashboardData([FromBody] GetDashboardData.Command data)
+//    {
+//        var result = await Mediator.Send(data);
 
-        return HttpResponse(result);
-    } 
-}
+//        return HttpResponse(result);
+//    } 
+//}
