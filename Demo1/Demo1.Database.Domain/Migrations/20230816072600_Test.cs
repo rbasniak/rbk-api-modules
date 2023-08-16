5121ddd0b21ef9be1c906e2c09efb36cf0f0b485
Rodrigo Basniak <rbasniak@gmail.com>
2023-08-16 09:26:48 +0200

    Added migration to Demo1

---
+++ Demo1/Demo1.Database.Domain/Migrations/20230816072600_Test.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Demo1.Database.Domain.Migrations
+{
+    /// <inheritdoc />
+    public partial class Test : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "DisplayName",
+                table: "Users",
+                type: "character varying(255)",
+                maxLength: 255,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "character varying(32)",
+                oldMaxLength: 32,
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Metadata",
+                table: "Comments",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Metadata",
+                table: "Comments");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "DisplayName",
+                table: "Users",
+                type: "character varying(32)",
+                maxLength: 32,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "character varying(255)",
+                oldMaxLength: 255,
+                oldNullable: true);
+        }
+    }
+}
